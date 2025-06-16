48894bb2a14128bf0a9999f8a6f02883d30027ba
cyso <hieunguyenhuy20@gmail.com>
2025-06-17 00:45:05 +0700

    last update

--- src/components/resources/Loading.jsx
+++ src/components/resources/Loading.jsx
@@ -3,7 +3,7 @@ import { CircularProgress, Box } from "@mui/material";
 export default function Loading() {
     return (
         <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-            <CircularProgress /> 
+            <CircularProgress />
         </Box>
     );
 }
